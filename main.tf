3bae2adfa39941b5089b3228e371f538f49fbf83
teun_ <teun_@DESKTOP-81EBD61>
2021-02-15 19:17:42 +0100

    no version

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "google_container_cluster" "default" {
   name               = "silvester-cluster"
   location           = "europe-west1-b" # MUST BE A SINGLE ZONE, OTHERWISE IT COUNTS AS A REGIONAL CLUSTER
   min_master_version = "version"
-  version = "1.0.0"
-  
+
+
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
@@ -18,7 +18,6 @@ resource "google_container_cluster" "default" {
 resource "google_container_node_pool" "memory_optimized" {
   name     = "silvester-nodepool"
   cluster  = google_container_cluster.default.name
-  version = "1.0.0"
   initial_node_count = 1
 
   autoscaling {
