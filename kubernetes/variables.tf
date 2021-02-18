12014552693a98036175c73327e8ffe79a485e43
teun_ <teun_@DESKTOP-81EBD61>
2021-02-18 21:52:15 +0100

    Separat efiles.

--- kubernetes/variables.tf
+++ kubernetes/variables.tf
@@ -3,7 +3,16 @@ variable "project" {
     default = "silvester-304916"
 }
 
+variable "ingress_namespace" {
+    type = string
+    default = "ingress-nginx"
+}
+
 variable "cluster_location" {
     type = string
     default = "europe-west1-b"
+}
+
+variable "kubeconfig_path" {
+    type = string
 }
