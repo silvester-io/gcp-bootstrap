eb302f0dfd626c1b510f09df5d852806e8fe2c3b
teun_ <teun_@DESKTOP-81EBD61>
2021-02-18 21:25:19 +0100

    Separat efiles.

--- kubernetes/ingress.tf
+++ kubernetes/ingress.tf
@@ -3,11 +3,7 @@
 data "google_client_config" "provider" {}
 
 provider "kubernetes" {
-  host  = "https://${google_container_cluster.silvester_cluster.endpoint}"
-  token = data.google_client_config.provider.access_token
-  cluster_ca_certificate = base64decode(
-    google_container_cluster.silvester_cluster.master_auth[0].cluster_ca_certificate,
-  )
+  config_path = "KUBECONFIG"
 }
 
 #INGRESS CONTROLLER
