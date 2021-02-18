680bd1ce9e51ef4ce00b0b56dcd03b2342b75d12
teun_ <teun_@DESKTOP-81EBD61>
2021-02-18 20:38:11 +0100

    Separat efiles.

--- nginx-ingress-controller.tf
+++ nginx-ingress-controller.tf
@@ -2,6 +2,14 @@
 #SETUP KUBECTL
 data "google_client_config" "provider" {}
 
+provider "kubernetes" {
+  host  = "https://${google_container_cluster.silvester_cluster.endpoint}"
+  token = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.silvester_cluster.master_auth[0].cluster_ca_certificate,
+  )
+}
+
 #INGRESS CONTROLLER
 resource "kubernetes_namespace" "ingress_nginx" {
   metadata {
