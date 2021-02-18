f857b7c810d9e4e687e4e0d1839328e08f58af21
teun_ <teun_@DESKTOP-81EBD61>
2021-02-18 21:15:11 +0100

    Separat efiles.

--- ingress/main.tf
+++ ingress/main.tf
@@ -4,6 +4,10 @@ data "google_client_config" "provider" {}
 
 provider "kubernetes" {
   host  = "https://${google_container_cluster.silvester_cluster.endpoint}"
+  token = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.silvester_cluster.master_auth[0].cluster_ca_certificate,
+  )
 }
 
 #INGRESS CONTROLLER
