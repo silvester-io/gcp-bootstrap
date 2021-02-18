a5767103cb72cafeccaed09c7abfb7ec90fc637a
teun_ <teun_@DESKTOP-81EBD61>
2021-02-18 17:56:54 +0100

    fix

--- main.tf
+++ main.tf
@@ -76,7 +76,7 @@ provider "kubernetes" {
   host  = "https://${google_container_cluster.silvester_cluster.endpoint}"
   token = data.google_client_config.provider.access_token
   cluster_ca_certificate = base64decode(
-    data.google_container_cluster.silvester_cluster.master_auth[0].cluster_ca_certificate,
+    google_container_cluster.silvester_cluster.master_auth[0].cluster_ca_certificate,
   )
 }
 
