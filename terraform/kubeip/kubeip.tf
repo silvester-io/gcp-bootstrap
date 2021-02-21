16356932b9917048d17815cab13867c8b23f9cba
teun_ <teun_@DESKTOP-81EBD61>
2021-02-21 18:21:56 +0100

    KubeIP ServiceAccount

--- terraform/kubeip/kubeip.tf
+++ terraform/kubeip/kubeip.tf
@@ -26,7 +26,7 @@ resource "google_project_iam_binding" "kubeip_role_binding" {
 
 # IAM Policy Binding
 resource "google_service_account_iam_binding" "kubeip_iam_policy_binding" {
-  service_account_id = "${var.kubeip_google_serviceaccount_name}@${var.project}.iam.gserviceaccount.com"
+  service_account_id = google_service_account.kubeip_service_account.name
   role    = "roles/iam.workloadIdentityUser"
 
   members = [
