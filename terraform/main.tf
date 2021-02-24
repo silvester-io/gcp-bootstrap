96eadf8bdb1fc395ce8c6bc4408575de2aa07122
teun_ <teun_@DESKTOP-81EBD61>
2021-02-24 23:52:54 +0100

    input variables.

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,19 @@ provider "google" {
 
 module "cluster" {
     source = "./cluster"
+
+    project = var.project
+    network = var.network
+    cluster_location = var.cluster_location
+    cluster_region = var.cluster_region
+    cluster_name = var.cluster_name
+    subnet_name = var.subnet_name
+    pods_ip_range_name = var.pods_ip_range_name
+    services_ip_range_name = ver.services_ip_range_name
 }
 
 resource "kubernetes_namespace" "namespace_ingress_nginx" {
-  name = "ingress-nginx"
+    metadata {
+      name = "ingress-nginx"
+    }
 }
