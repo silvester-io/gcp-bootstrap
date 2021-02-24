0cb221e674584bd6c9c37d917ca7486e86fa750b
teun_ <teun_@DESKTOP-81EBD61>
2021-02-24 23:54:13 +0100

    input variables.

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ module "cluster" {
     cluster_name = var.cluster_name
     subnet_name = var.subnet_name
     pods_ip_range_name = var.pods_ip_range_name
-    services_ip_range_name = ver.services_ip_range_name
+    services_ip_range_name = var.services_ip_range_name
 }
 
 resource "kubernetes_namespace" "namespace_ingress_nginx" {
