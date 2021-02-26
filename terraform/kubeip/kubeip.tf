c156af3ff1037f1af3ff8d735ba139c956db65ee
teun_ <teun_@DESKTOP-81EBD61>
2021-02-26 18:50:38 +0100

    cluster-admin

--- terraform/kubeip/kubeip.tf
+++ terraform/kubeip/kubeip.tf
@@ -107,7 +107,7 @@ resource "kubernetes_cluster_role_binding" "kubeip" {
   role_ref {
     api_group = "rbac.authorization.k8s.io"
     kind      = "ClusterRole"
-    name      = kubernetes_cluster_role.kubeip.metadata.0.name
+    name      = "cluster-admin"
   }
   subject {
     kind      = "ServiceAccount"
